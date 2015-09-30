cf946f34bcbd61822b129140619f2f306c206df4
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2015-09-30 08:34:26 +0100

    [ADD] App Style

---
+++ TrendingVenues/Application/AppStyle.m
@@ -0,0 +1,36 @@
+//
+//  AppStyle.m
+//  TrendingVenues
+//
+//  Created by Martin Lloyd on 29/09/2015.
+//  Copyright © 2015 mlloyd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppStyle.h"
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+@implementation AppStyle
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
++ (void)applyAppStyle
+{
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+        
+    [[UINavigationBar appearance] setTranslucent:NO];
+    [[UINavigationBar appearance] setBarTintColor:[AppStyle purple]];
+    [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    [[UISearchBar     appearance] setBarTintColor:[AppStyle purple]];
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
++ (UIColor *)purple
+{
+    return [UIColor colorWithRed:0.463f green:0.239f blue:0.518f alpha:1.00f];
+}
+
+@end
